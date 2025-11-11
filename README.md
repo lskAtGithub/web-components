3d82288cf0276014e783752ed34e7c687e05b158
liushengkai <liushengkai95@gmail.com>
2025-11-11 14:08:28 +0800

    feat: project init

--- README.md
+++
@@ -1,4 +0,0 @@
-
-## web components
-
- 使用 monorepo 开发的 web 组件， 基于` vite & ts & tailwind ` 开发
--- packages/styles/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: ['../components/src/**/*.{ts,js}'],
-  theme: {
-    extend: {}
-  },
-  corePlugins: {
-    preflight: false // ❗️ 不重置 shadow dom
-  }
-};
